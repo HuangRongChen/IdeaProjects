6d08b202690409ee1448df724e91f79778db4ec5
黄荣臣 <1371973444@qq.com>
2019-07-15 00:09:21 +0800

    增加转账实例程序

---
+++ code005/src/zhuanzhang/People.java
@@ -0,0 +1,30 @@
+package zhuanzhang;
+
+public class People {
+    // 姓名
+    private String name;
+    // 金额
+    private double balance;
+
+    public People(String name, double balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+}
