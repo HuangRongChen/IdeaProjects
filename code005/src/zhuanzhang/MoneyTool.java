242d925ab493718e6c10af895ca1c98972143170
黄荣臣 <1371973444@qq.com>
2019-07-15 00:14:58 +0800

    重构代码，抽取转账工具类

---
+++ code005/src/zhuanzhang/MoneyTool.java
@@ -0,0 +1,29 @@
+package zhuanzhang;
+
+/**
+ * 钱包工具
+ */
+public class MoneyTool {
+
+    /**
+     * 发起转账
+     *
+     * @param from   转账发起人
+     * @param to     收款人
+     * @param amount 转账金额
+     */
+    public static void send(People from, People to, double amount) {
+        if (from.getBalance() < amount) {
+            System.out.println(from.getName() + "余额不足");
+            return;
+        }
+
+        // 1、扣减转账发起人余额
+        double value = from.getBalance() - amount;
+        from.setBalance(value);
+
+        // 2、收款人余额增加
+        to.setBalance(to.getBalance() + amount);
+    }
+
+}
--- code005/src/zhuanzhang/PeopleTest.java
+++ code005/src/zhuanzhang/PeopleTest.java
@@ -8,32 +8,11 @@ public class PeopleTest {
         System.out.println(P1.getName() + "收款前 " + P1.getBalance());
         System.out.println(P2.getName() + "转账前 " + P2.getBalance());
 
-        send(P2, P1, 10);
-        send(P2, P1, 10);
+        MoneyTool.send(P2, P1, 10);
+        MoneyTool.send(P2, P1, 10);
 
         System.out.println(P1.getName() + "收款后 " + P1.getBalance());
         System.out.println(P2.getName() + "转账后 " + P2.getBalance());
-    }
-
-    /**
-     * 发起转账
-     *
-     * @param from   转账发起人
-     * @param to     收款人
-     * @param amount 转账金额
-     */
-    public static void send(People from, People to, double amount) {
-        if (from.getBalance() < amount) {
-            System.out.println(from.getName() + "余额不足");
-            return;
-        }
-
-        // 1、扣减转账发起人余额
-        double value = from.getBalance() - amount;
-        from.setBalance(value);
-
-        // 2、收款人余额增加
-        to.setBalance(to.getBalance() + amount);
     }
 
 }
